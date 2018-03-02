f247ddc3cbcb36e70c91659b550644ac5ebb5442
Zachary Roberts <rober@ZAK-LAPTOP.stonewallgroup.local>
2018-03-01 20:49:47 -0500

    can go down directories now:

--- public/js/main.js
+++ public/js/main.js
@@ -7,54 +7,79 @@ const $ = require("jquery");
 let start_path = os.homedir() + "/Desktop";
 start_path = start_path.replace(/\\/g, '/') + '/';
 
-let files = [];
-files = fetchDirectory(start_path);
-buildDirectory(files);
+buildDirectory(start_path);
 
 
-function icoStringBuilder(a){
-	return  `<div class="ico" data-type="${a}"></div>`;
-	return `<div class="ico" data-type="${a}"><img src="/assets/icons/${a}.png" alt="${a}" /></div>`;
+function doubleClickFunction(_theItem){
+	console.log(_theItem);
+	if(_theItem.slice(-1) == "/"){
+		buildDirectory(_theItem);
+	}else{
+		console.log("it's a file!");
+	}
+	/*if($(this).attr("data-type") == "folder"){
+		console.log("A Folder!");
+	}else{
+		console.log("not a folder....");
+	}*/
 }
+
 function fnStringBuilder(a, b){
-	return  `<div class="fn"><a href="" data-type="${b}">${a}</a></div>`;
+	return  `<div class="file" data-type="${b}" ondblclick="doubleClickFunction('${a.path}${a.theFile}${a.directory ? '/':'' }')"><i class="ico ${b}" data-type="${b}"><img src="assets/icons/${b}.png" alt="${b}" /></i>${a.theFile}</div>`;
+}
+function mimeStringBuilder(a){
+	return `<div class"mime">${a}</div>`;
 }
 function buildDirectory(_thePath){
-	if(_thePath.length > 1 ){ 
-		_thePath.forEach(fileObject => {
+	let _theFiles = fetchDirectory(_thePath);
+	$('.file-container').html("");
+	if(_theFiles.length > 1 ){ 
+		_theFiles.forEach(fileObject => {
 			let stringBuilder = `<div class="item">`;
 			fileType = "";
 			if(fileObject.directory){
-				stringBuilder += icoStringBuilder("folder");
 				fileType = "folder";
 			}else if(fileObject.alias){
-				stringBuilder += icoStringBuilder("alias");
 				fileType = "alias";
 			}else{
-				stringBuilder += icoStringBuilder("file");
-				fileType = fileType.mimeType;
+				fileType = "file";
 			}
-			stringBuilder += fnStringBuilder(fileObject.theFile, fileType);
-			console.log(fileObject.theFile + " " + fileObject.mimeType);
+			stringBuilder += fnStringBuilder(fileObject, fileType);
+			if(!(fileObject.directory) && !(fileObject.alias)){
+				stringBuilder += mimeStringBuilder(fileObject.mimeType);
+			}else{
+				stringBuilder += mimeStringBuilder(" ");
+			}
 			stringBuilder += `</div>`;
 			$('.file-container').append(stringBuilder);
 		});
 	}else{
-		console.log("files is empty: " + array.length);
+		console.log("files is empty: " + _theFiles.length);
 	}
 }
 function fetchDirectory(_thePath){
-	arr = [];
-	files = fs.readdirSync(_thePath)
-		
+	let arr = [];
+	let fileArr = [];
+	let folderArr = [];
+	let files = fs.readdirSync(_thePath)	
 	files.forEach(file => {
-		arr.push({
-			path: _thePath,
-			theFile: file,
-			directory: fs.statSync(_thePath + file).isDirectory(),
-			alias: fs.statSync(_thePath + file).isSymbolicLink(),
-			mimeType: mime.getType(file)
-		});
+		if( fs.statSync(_thePath + file).isDirectory() ){
+			folderArr.push({
+				path: _thePath,
+				theFile: file,
+				directory: fs.statSync(_thePath + file).isDirectory(),
+				alias: fs.statSync(_thePath + file).isSymbolicLink(),
+				mimeType: mime.getType(file)
+			});
+		}else{	
+			fileArr.push({
+				path: _thePath,
+				theFile: file,
+				directory: fs.statSync(_thePath + file).isDirectory(),
+				alias: fs.statSync(_thePath + file).isSymbolicLink(),
+				mimeType: mime.getType(file)
+			});
+		}
 	});
-	return arr;
+	return folderArr.concat(fileArr);
 }
